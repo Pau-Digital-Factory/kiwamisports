420068b4d3c242bb196f1ad632ec8708b81a27c3
abdelghani-khalidi <51909290+abdelghani-khalidi@users.noreply.github.com>
2021-12-30 16:42:55 +0100

    Update model.py

--- custom_kiwami/models/model.py
+++ custom_kiwami/models/model.py
@@ -4,6 +4,82 @@ from odoo.exceptions import AccessError
 from odoo.addons.shopify_ept import shopify
 
 
+
+class Shopifysale.order.line(models.Model):
+     _inherit = "sale.order.line"
+     def get_sale_order_line_multiline_description_sale(self, product):
+        """ Compute a default multiline description for this sales order line.
+
+        In most cases the product description is enough but sometimes we need to append information that only
+        exists on the sale order line itself.
+        e.g:
+        - custom attributes and attributes that don't create variants, both introduced by the "product configurator"
+        - in event_sale we need to know specifically the sales order line as well as the product to generate the name:
+          the product is not sufficient because we also need to know the event_id and the event_ticket_id (both which belong to the sale order line).
+        """
+        
+        
+        if not self.product_id:
+            return ''
+
+        color = ""
+        taille = ""
+        if self.product_id.product_template_variant_value_ids:
+          for m in self.product_id.product_template_variant_value_ids:
+            phrase = m.attribute_id.name
+            if m.attribute_id.id in [2,3,4]:
+                color = "Color: "+ m.name
+            else:
+                taille = m.name
+
+        values = []
+        if self.product_id.type_product:
+            type_perso = str(self.product_id.type_product)
+        else:
+            type_perso = " "
+        if self.product_id.product_template_variant_value_ids:
+           lieu = self.product_id.name +" \n \n Taille :"+taille+" QTY: "+str(self.product_uom_qty)+" \n \n"+type_perso +", Type : "+self.product_id.name + color +", made in France by Kiwami 9 rue ampere 64121 Montardon"
+        else: 
+          lieu = " "
+          if product.partner_ref:
+             values.append(product.partner_ref)
+        values.append(lieu)    
+        if product.description_sale: 
+                values.append(product.description_sale)
+        return '\n'.join(values)
+
+     @api.onchange('product_uom_qty')
+     def _onchange_quantity_desc(self):
+          if not self.product_id:
+            return ''
+
+        color = ""
+        taille = ""
+        if self.product_id.product_template_variant_value_ids:
+          for m in self.product_id.product_template_variant_value_ids:
+            phrase = m.attribute_id.name
+            if m.attribute_id.id in [2,3,4]:
+                color = "Color: "+ m.name
+            else:
+                taille = m.name
+
+        values = []
+        if self.product_id.type_product:
+            type_perso = str(self.product_id.type_product)
+        else:
+            type_perso = " "
+        if self.product_id.product_template_variant_value_ids:
+           lieu = self.product_id.name +" \n \n Taille :"+taille+" QTY: "+str(self.product_uom_qty)+" \n \n"+type_perso +", Type : "+self.product_id.name + color +", made in France by Kiwami 9 rue ampere 64121 Montardon"
+        else: 
+          lieu = " "
+          if product.partner_ref:
+             values.append(product.partner_ref)
+        values.append(lieu)    
+        if product.description_sale: 
+                values.append(product.description_sale)
+        return '\n'.join(values)
+
+
 class ShopifyProductProducttemplate(models.Model):
      _inherit = "product.template"
      type_product = fields.Char()
