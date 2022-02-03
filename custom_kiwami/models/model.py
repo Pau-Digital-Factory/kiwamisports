2e90e218b635056229960523d75138521da45e1a
abdelghani-khalidi <51909290+abdelghani-khalidi@users.noreply.github.com>
2022-02-03 12:27:07 +0100

    Update model.py

--- custom_kiwami/models/model.py
+++ custom_kiwami/models/model.py
@@ -21,6 +21,8 @@ class Shopifysale_order_line(models.Model):
         
         if not self.product_id:
             return ''
+        if self.order_id.partner_id.country_id.code == "FR":
+               return product.get_product_multiline_description_sale() + self._get_sale_order_line_multiline_description_variants()
 
         color = ""
         taille = ""
@@ -48,36 +50,40 @@ class Shopifysale_order_line(models.Model):
                 values.append(product.description_sale)
         return '\n'.join(values)
 
-#      @api.onchange('product_uom_qty')
-#      def _onchange_quantity_desc(self):
-#         if not self.product_id:
-#             return ''
-#         product = self.product_id
-#         color = ""
-#         taille = ""
-#         if self.product_id.product_template_variant_value_ids:
-#           for m in self.product_id.product_template_variant_value_ids:
-#             phrase = m.attribute_id.name
-#             if m.attribute_id.id in [2,3,4]:
-#                 color = "Color: "+ m.name
-#             else:
-#                 taille = m.name
+      @api.onchange('product_uom_qty')
+      def _onchange_quantity_desc_for_export(self):
+       if not self.product_id:
+             self.name = ''
+       if self.order_id.partner_id.country_id.code == "FR":
+               self.name =  str(self.product_id.get_product_multiline_description_sale()) + str(self._get_sale_order_line_multiline_description_variants())
+       else:
 
-#         values = []
-#         if self.product_id.type_product:
-#             type_perso = str(self.product_id.type_product)
-#         else:
-#             type_perso = " "
-#         if self.product_id.product_template_variant_value_ids:
-#            lieu = self.product_id.name +" \n \n Taille :"+taille+" QTY: "+str(self.product_uom_qty)+" \n \n"+type_perso +", Type : "+self.product_id.name + color +", made in France by Kiwami 9 rue ampere 64121 Montardon"
-#         else: 
-#           lieu = " "
-#           if self.product_id.partner_ref:
-#              values.append(self.product_id.partner_ref)
-#         values.append(lieu)    
-#         if self.product_id.description_sale: 
-#                 values.append(self.product_id.description_sale)
-#         return '\n'.join(values)
+        color = ""
+        taille = ""
+        if self.product_id.product_template_variant_value_ids:
+          for m in self.product_id.product_template_variant_value_ids:
+            phrase = m.attribute_id.name
+            if m.attribute_id.id in [2,3,4]:
+                color = ", Color: "+ m.name
+            else:
+                taille = m.name
+
+        values = []
+        if self.product_id.type_product:
+            type_perso = str(self.product_id.type_product)
+        else:
+            type_perso = " "
+        if self.product_id.product_template_variant_value_ids:
+           lieu = self.product_id.name +" \n \n Taille :"+taille+" QTY: "+str(self.product_uom_qty)+" \n \n"+type_perso +", Type : "+self.product_id.name + color +", made in France by Kiwami 9 rue ampere 64121 Montardon"
+        else: 
+          lieu = " "
+          if self.product_id.partner_ref:
+             values.append(self.product_id.partner_ref)
+        values.append(lieu)    
+        if self.product_id.description_sale: 
+                values.append(self.product_id.description_sale)
+        self.name =  '\n'.join(values)
+
 
 
 class ShopifyProductProducttemplate(models.Model):
@@ -108,10 +114,32 @@ class Shopifysaleorderline(models.Model):
         if not self.product_id:
             return ''
 
+
         if self.partner_id.lang:
             product = self.product_id.with_context(lang=self.partner_id.lang)
         else:
             product = self.product_id
+               
+        
+     
+     
+        if self.partner_id.country_id.code == "FR":
+          
+          values2 = []
+          if product.partner_ref:
+            values2.append(product.partner_ref)
+          if self.journal_id.type == 'sale':
+            if product.description_sale:
+                values2.append(product.description_sale)
+          elif self.journal_id.type == 'purchase':
+            if product.description_purchase:
+                values2.append(product.description_purchase)
+          return '\n'.join(values2)
+          
+          
+          
+          
+          
         color = ""
         taille = ""
         if self.product_id.product_template_variant_value_ids:
@@ -144,7 +172,57 @@ class Shopifysaleorderline(models.Model):
                 values.append(product.description_purchase)
         return '\n'.join(values)
 
+     @api.onchange('partner_id')
+     def _onchange_partner_fr(self):
+        self.ensure_one()
 
+        if not self.product_id:
+            return ''
+
+        if self.partner_id.lang:
+            product = self.product_id.with_context(lang=self.partner_id.lang)
+        else:
+            product = self.product_id
+               
+        if self.partner_id.country_id.code == "FR":
+          
+          values2 = []
+          if product.partner_ref:
+            values2.append(product.partner_ref)
+          if self.journal_id.type == 'sale':
+            if product.description_sale:
+                values2.append(product.description_sale)
+          elif self.journal_id.type == 'purchase':
+            if product.description_purchase:
+                values2.append(product.description_purchase)
+          return '\n'.join(values2)
+        color = ""
+        taille = ""
+        if self.product_id.product_template_variant_value_ids:
+         for m in self.product_id.product_template_variant_value_ids:
+            phrase = m.attribute_id.name
+            if m.attribute_id.id in [2,3,4]:
+                color = m.name
+            else:
+                taille = m.name
+
+        values = []
+        if self.product_id.product_template_variant_value_ids:
+           lieu = self.product_id.name +" \n \n Taille :"+taille+" QTY: "+str(self.quantity)+" \n \n"+str(self.product_id.type_product) +", Type : "+self.product_id.name+", Color : "+ color +", made in France by Kiwami 9 rue ampere 64121 Montardon"
+        else: 
+          lieu = " "
+          if product.partner_ref:
+             values.append(product.partner_ref)
+        values.append(lieu)    
+        if self.journal_id.type == 'sale':
+            if product.description_sale:
+                
+                values.append(product.description_sale)
+                
+        elif self.journal_id.type == 'purchase':
+            if product.description_purchase:
+                values.append(product.description_purchase)
+        self.name =  '\n'.join(values)
      @api.onchange('quantity')
      def _onchange_quantity_desc(self):
         self.ensure_one()
@@ -156,6 +234,19 @@ class Shopifysaleorderline(models.Model):
             product = self.product_id.with_context(lang=self.partner_id.lang)
         else:
             product = self.product_id
+               
+        if self.partner_id.country_id.code == "FR":
+          
+          values2 = []
+          if product.partner_ref:
+            values2.append(product.partner_ref)
+          if self.journal_id.type == 'sale':
+            if product.description_sale:
+                values2.append(product.description_sale)
+          elif self.journal_id.type == 'purchase':
+            if product.description_purchase:
+                values2.append(product.description_purchase)
+          return '\n'.join(values2)
         color = ""
         taille = ""
         if self.product_id.product_template_variant_value_ids:
