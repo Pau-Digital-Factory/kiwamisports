46a0ee4b6618ffc6431233b72bfc0623f42bf873
LonguetMax <maxime@paudigitalfactory.fr>
2022-07-11 11:48:43 +0200

    Test : Apparition du champs Position Fiscale dans la list view des factures client

---
+++ search_filters/__manifest__.py
@@ -0,0 +1,15 @@
+# See LICENSE file for full copyright and licensing details.
+
+{
+    "name": "Invoice Search Filters",
+    "version": "1.0",
+    "category": "Accounting",
+    "summary": "Custom invoices search filters",
+    "author": "PauDigitalFactory",
+    "depends": [
+        "account",
+    ],
+    "data": [
+        "views/account_move_views_custom.xml",
+    ],
+}
