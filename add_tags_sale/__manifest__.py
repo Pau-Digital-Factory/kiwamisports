7dff100b6560fe6c42f2c37aca7c733157289151
abdelghani-khalidi <51909290+abdelghani-khalidi@users.noreply.github.com>
2022-12-02 15:13:25 +0100

    Add files via upload

---
+++ add_tags_sale/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2016-2017 Akretion (http://www.akretion.com)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'add tags sale',
+    'version': '15.0.1.0.0',
+    'summary': 'Technical module Add tags in sale reports '
+    ' State',
+    'author': 'Abdelghani KHALIDI',
+    'website': 'https://DKGROUP.FR',
+    'license': 'AGPL-3',
+    'category': 'Generic Modules',
+    'depends': ['base','sale'],
+    'installable': True,
+}
---
+++ add_tags_sale/models/add_tags_sale_inherit.py
@@ -0,0 +1,15 @@
+from odoo import tools
+from odoo import api, fields, models
+
+
+class SaleReport(models.Model):
+    _name = "sale.report"
+
+    category_id = fields.Many2many('res.partner.category', column1='partner_id',
+                                    column2='category_id', string='Tags', readonly=True)
+
+    def _query(self, with_clause='', fields={}, groupby='', from_clause=''):
+              fields['category_id'] = ", s.category_id as category_id" groupby += ', s.category_id '
+              return super(ClassName, self)._query(with_clause, fields, groupby, from_clause)
+
+
