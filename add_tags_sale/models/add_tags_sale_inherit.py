e1216e001a72eee97b8a899da619fb7ad5b28cd2
abdelghani-khalidi <51909290+abdelghani-khalidi@users.noreply.github.com>
2022-12-02 16:21:11 +0100

    Update add_tags_sale_inherit.py

--- add_tags_sale/models/add_tags_sale_inherit.py
+++ add_tags_sale/models/add_tags_sale_inherit.py
@@ -25,3 +25,26 @@ class SaleReportv(models.Model):
               return super(SaleReportv, self)._query(with_clause, fields, groupby, from_clause)
 
 
+class AccountMove(models.Model):
+    _inherit = "account.move"
+    tagg = fields.Char('Tags', compute='_compute_tagg', store=True)
+    
+    @api.depends('partner_id.category_id')
+    def _compute_tagg(self):
+        for record in self:
+            if record.partner_id.category_id:
+                record.tagg = record.partner_id.category_id[0].name
+            else:
+                record.tagg = False
+    
+
+class AccountInvoiceReportv(models.Model):
+    _inherit = "account.invoice.report"
+    tagg = fields.Char('Tags', readonly=True)
+    
+
+    def _query(self, with_clause='', fields={}, groupby='', from_clause=''):
+              fields['tagg'] = ", s.tagg as tagg" 
+              groupby += ', s.tagg '
+              return super(AccountInvoiceReportv, self)._query(with_clause, fields, groupby, from_clause)
+
