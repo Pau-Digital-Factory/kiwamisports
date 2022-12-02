cd590db2c4b9437b4eac69283ada7175d20b4d91
abdelghani-khalidi <51909290+abdelghani-khalidi@users.noreply.github.com>
2022-12-02 15:54:56 +0100

    Update add_tags_sale_inherit.py

--- add_tags_sale/models/add_tags_sale_inherit.py
+++ add_tags_sale/models/add_tags_sale_inherit.py
@@ -4,7 +4,7 @@ from odoo import api, fields, models
 
 class Saleorder(models.Model):
     _inherit = "sale.order"
-    tagg = fields.Char('Tags', compute='_compute_tagg')
+    tagg = fields.Char('Tags', compute='_compute_tagg', store=True)
     
     @api.depends('partner_id')
     def _compute_tagg(self):
