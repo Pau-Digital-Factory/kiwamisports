b6ff524ed658ca17ca3bfd49f49d9e641a5edeb6
abdelghani-khalidi <51909290+abdelghani-khalidi@users.noreply.github.com>
2022-12-02 15:37:19 +0100

    Update add_tags_sale_inherit.py

--- add_tags_sale/models/add_tags_sale_inherit.py
+++ add_tags_sale/models/add_tags_sale_inherit.py
@@ -2,15 +2,26 @@ from odoo import tools
 from odoo import api, fields, models
 
 
+class Saleorder(models.Model):
+    _name = "sale.order"
+    tagg = fields.Char('Tags', compute='_compute_tagg')
+    
+    @api.depends('partner_id')
+    def _compute_tagg(self):
+        for record in self:
+            if reocrd.partner_id.category_id[0]:
+                record.tagg = reocrd.partner_id.category_id[0].name
+            else:
+                record.tagg = False
+    
 class SaleReport(models.Model):
     _name = "sale.report"
-
-    category_id = fields.Many2many('res.partner.category', column1='partner_id',
-                                    column2='category_id', string='Tags', readonly=True)
+    tagg = fields.Char('Tags', readonly=True)
+    
 
     def _query(self, with_clause='', fields={}, groupby='', from_clause=''):
-              fields['category_id'] = ", s.category_id as category_id" 
-              groupby += ', s.category_id '
+              fields['tagg'] = ", s.tagg as tagg" 
+              groupby += ', s.tagg '
               return super(ClassName, self)._query(with_clause, fields, groupby, from_clause)
 
 
